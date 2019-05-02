4d5dc000da81a5fbce7e7dc3bcb49fce3956d6ab
Michael Campion <michael.campion@emutex.com>
2019-05-02 17:13:12 +0100

    up-fpga:dmi: Update DMI match date string
    This matches the DMI DATE DATA for UP3 BIOS name UPW1AM0B(04/11/2019)

--- drivers/pinctrl/pinctrl-upboard.c
+++ drivers/pinctrl/pinctrl-upboard.c
@@ -842,7 +842,7 @@ static const struct dmi_system_id upboard_dmi_table[] __initconst = {
 	{
 		.matches = { /* UP Xtreme */
 			DMI_EXACT_MATCH(DMI_SYS_VENDOR, "AAEON"),
-			DMI_EXACT_MATCH(DMI_BIOS_DATE, "03/18/2019"),
+			DMI_EXACT_MATCH(DMI_BIOS_DATE, "04/11/2019"),
 		},
 		.driver_data = (void *)&upboard_up_bios_info_dvt,
 	},
